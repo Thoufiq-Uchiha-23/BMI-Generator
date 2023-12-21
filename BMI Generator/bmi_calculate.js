b5099f8b7cb9a3c8e6358c97c7e2d2321d17cba9
Thoufiq-Uchiha-23 <143873191+Thoufiq-Uchiha-23@users.noreply.github.com>
2023-12-21 22:11:39 +0530

    first commit

---
+++ BMI Generator/bmi_calculate.js
@@ -0,0 +1,34 @@
+const form = document.querySelector('form');
+// this usecase will give you empty
+// const height = parseInt(document.querySelector('#height').value);
+
+form.addEventListener('submit', function (e) {
+  // Wait for a moment don't submit directly
+  e.preventDefault();
+
+  const height = parseInt(document.querySelector('#height').value);
+  const weight = parseInt(document.querySelector('#weight').value);
+  const results = document.querySelector('#results');
+  const guide = document.querySelector('#guide');
+  const output = document.querySelector('#output');
+
+  if (height == '' || height < 0 || isNaN(height)) {
+    results.innerHTML = `Please give a valid height ${height}`;
+  } else if (weight == '' || weight < 0 || isNaN(weight)) {
+    results.innerHTML = `Please give a valid weight ${weight}`;
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    // show the result
+    results.innerHTML = `<span>${bmi}</span>`;
+    if (bmi <= 18.6) {
+      guide.innerHTML = `You are in Under weight`;
+    }
+    if (bmi >= 18.6 && bmi <= 24.9) {
+      guide.innerHTML = `You are in Normal weight`;
+    }
+    if (bmi >= 24.9) {
+      guide.innerHTML = `You are in Greater weight`;
+    }
+  }
+  output.style.display = 'block';
+});
